df75b7d6b977dd454dae4c94bf26fbf12aa13a1e
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2021-09-14 18:48:44 -0700

    major cleanup; new reader structure and rewrites

--- gen_html.R
+++ gen_html.R
@@ -3,6 +3,3 @@
 # Settings for bookdown are in `_bookdown.yml` and settings for pandoc are in
 # `chapters/01_intro.Rmd`.
 bookdown::render_book("index.Rmd")
-
-#rmarkdown::render("./index.Rmd", output_dir="./docs/", output_file="index.html")
-
